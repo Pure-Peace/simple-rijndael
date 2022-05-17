bf2f401fd384682a13f7f27e824f8e14754860d5
mr-sven <sven.fabricius@livediesel.de>
2022-05-17 09:51:32 +0200

    Implement std error for Custom error

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,15 @@ pub enum Errors {
     InvalidBlockSize,
     InvalidKeySize,
 }
+
+impl std::error::Error for Errors {}
+
+impl std::fmt::Display for Errors {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Errors::InvalidDataSize => write!(f, "Invalid Data Size"),
+            Errors::InvalidBlockSize => write!(f, "Invalid Block Size"),
+            Errors::InvalidKeySize => write!(f, "Invalid Key Size"),
+        }
+    }
+}
